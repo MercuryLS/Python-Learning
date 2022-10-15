eee68da5da39c92ee97f69dc127ca20f0ee20a41
MercuryLS <111221528+MercuryLS@users.noreply.github.com>
2022-10-15 20:03:12 +0800

    Add files via upload

---
+++ 2022-10-15/2-8.cpp
@@ -0,0 +1,10 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+    cout << 'A'<< ' '<<'a'<<endl;
+    cout << "one\ttwo\tthree\n";
+    cout << "123\b\b45\n";//退格键
+    cout << "Alert\a\n";//响铃键
+    return 0;
+}
