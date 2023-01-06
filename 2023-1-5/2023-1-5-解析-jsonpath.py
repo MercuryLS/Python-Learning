9ddfe49433cf127b094f50572226b242faf6e0ed
MercuryLS <111221528+MercuryLS@users.noreply.github.com>
2023-01-06 20:20:54 +0800

    Add files via upload

---
+++ 2023-1-5/2023-1-5-解析-jsonpath.py
@@ -0,0 +1,19 @@
+﻿import jsonpath
+import json
+obj=json.load(open('store.json','r'))
+# 注意store中没有中文字符不需要encoding='utf-8
+author_1ist=jsonpath.jsonpath(obj,'$.store.book[*].author')
+author_list2=jsonpath.jsonpath(obj,'$..author')
+all_=jsonpath.jsonpath(obj,'$.store.*')
+price_ist=jsonpath.jsonpath(obj,'$.store..price')
+# 第三本书
+book3=jsonpath.jsonpath(obj,'$..book[2]')
+# 最后一本书,这里是book的length写法,@反映了
+book=jsonpath.jsonpath(obj,'$..book[(@.length-1)]')
+# 前两本书
+book1_2=jsonpath.jsonpath(obj,'$..book[:2]')
+# 过滤含isbn的书，条件过滤需要在圆括号前加?
+book_list=jsonpath.jsonpath(obj,'$..book[?(@.isbn)]')
+# 超过十元的书
+book_list2=jsonpath.jsonpath(obj,'$..book[?(@.price>10)]')
+print(book_list2)
---
+++ 2023-1-5/2023-1-5-解析-jsonpath解析淘票票.py
@@ -0,0 +1,34 @@
+﻿import urllib.request
+import json
+import jsonpath
+url='https://dianying.taobao.com/cityAction.json?activityId&_ksTS=1672890657064_104&jsoncallback=jsonp105&action=cityAction&n_s=new&event_submit_doGetAllRegion=true'
+headers={
+# ':authority':'dianying.taobao.com',
+# ':method':'GET',
+# ':path':'/cityAction.json?activityId&_ksTS=1672890657064_104&jsoncallback=jsonp105&action=cityAction&n_s=new&event_submit_doGetAllRegion=true',
+# ':scheme':'https',
+'accept':'text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01',
+# 'accept-encoding':'gzip, deflate, br',
+'accept-language':'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+'bx-v':'2.2.3',
+'cookie':'cna=YHN6G0YkMBgCAXWboWi5KN/C; ariaDefaultTheme=undefined; t=8152315df338d5fff8458f2e92396b38; cookie2=1cb1b2968d738f25bdd91f500cddc37f; v=0; _tb_token_=e33e36a91eeee; isg=BFZW_-rYKZdVihx2dXCkoT4fpwxY95oxgGVkF8C_DTnUg_YdKIdMQfR9Gx9vK5JJ; l=fBrjOzmnT3kmgHfaBOfZFurza779LIRfguPzaNbMi9fP_D5H5aUOB67y-MLMCnGVesOyR3kbtB6WBzTG0yKNVcYON7h1WnqxndhyN3pR.',
+'referer':'https://dianying.taobao.com/',
+'sec-ch-ua':'"Not?A_Brand";v="8", "Chromium";v="108", "Microsoft Edge";v="108"',
+'sec-ch-ua-mobile':'?0',
+'sec-ch-ua-platform':'"Windows"',
+'sec-fetch-dest':'empty',
+'sec-fetch-mode':'cors',
+'sec-fetch-site':'same-origin',
+'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54',
+'x-requested-with':'XMLHttpRequest',
+}
+request=urllib.request.Request(url=url,headers=headers)
+response=urllib.request.urlopen(request)
+content=response.read().decode('utf-8')
+# split切割
+content=content.split('(')[1].split(')')[0]
+with open('data.json','w',encoding='utf-8') as fp:
+    fp.write(content)
+obj=json.load(open('data.json','r',encoding='utf-8'))
+city_list=jsonpath.jsonpath(obj,'$..regionName')
+print(city_list)
